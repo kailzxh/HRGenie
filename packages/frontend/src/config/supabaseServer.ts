19e9aaf437a042c61b15bab8e01a5a5c35810c24
kailzxh <137093796+kailzxh@users.noreply.github.com>
2025-10-21 13:45:35 +0530

    vercel deploy

--- packages/frontend/src/config/supabaseServer.ts
+++ packages/frontend/src/config/supabaseServer.ts
@@ -1,13 +1,13 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabaseServer = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY!
 );
 
 export const supabaseOnboarding = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_KEY!,
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY!,
   {
     db: { schema: 'onboarding' },
   }
